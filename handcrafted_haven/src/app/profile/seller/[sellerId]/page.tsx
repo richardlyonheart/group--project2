85f9ae28108c2851f7d435318ec2cecefc2fe174
richardlyonheart <brendenlyon907@gmail.com>
2025-06-12 10:11:59 -0400

    trababajando con vercel

--- handcrafted_haven/src/app/profile/seller/[sellerId]/page.tsx
+++ handcrafted_haven/src/app/profile/seller/[sellerId]/page.tsx
@@ -34,7 +34,7 @@ const mockSellerData = {
   },
 };
 
-export default async function SellerProfilePage({ params }: SellerProfilePageProps) {
+export default function SellerProfilePage({ params }: SellerProfilePageProps) {
   const { sellerId } = params;
 
   // En un escenario real, aquí harías una petición a tu API para obtener los datos del vendedor:
